3d4ecb4130913066ad03a76a7cdececbbae586bf
Ash Porter <porter.ashley13@gmail.com>
2024-07-24 13:54:16 -0700

    some changes

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"sort"
+	"strconv"
+
 	"github.com/rivo/tview"
 )
 
@@ -13,29 +16,41 @@ type Team struct {
 // Character struct
 type Character struct {
 	Name string
-	Init string
-	HP   string
+	Init int
+	HP   int
 	Team Team
 }
 
 var characters []Character
 
+// Add a new character to the list
 func AddNewCharacter(class Character) {
-	// Add a new character to the list
 	characters = append(characters, class)
+}
+
+func GetCharactersSorted() []Character {
+	// Sort the characters by initiative
+	sort.Slice(characters, func(i, j int) bool {
+		return characters[i].Init < characters[j].Init
+	})
+	return characters
 }
 
 func main() {
 	// The base application
 	app := tview.NewApplication()
 
-	headerBox := tview.NewBox()
-	displayList := tview.NewList()
-	list := tview.NewList()
+	// The components
 	addNewForm := tview.NewForm()
+	displayList := tview.NewList()
+	headerBox := tview.NewBox()
+	commandList := tview.NewList()
 
+	// Add styling
+	addNewForm.SetHorizontal(true).SetBorder(true).SetTitle(" Add New Line ")
+	displayList.SetBorder(true).SetTitle(" Initiative ")
 	headerBox.SetBorder(true).SetTitle(" Initiative Tracker ")
-	displayList.SetBorder(true).SetTitle(" Current ")
+	commandList.SetBorder(true).SetTitle(" Commands ")
 
 	addNewForm.AddInputField("Name", "", 20, nil, nil).
 		AddInputField("Init", "", 3, nil, nil).
@@ -45,43 +60,49 @@ func main() {
 			teamId, teamText := addNewForm.GetFormItemByLabel("Team").(*tview.DropDown).GetCurrentOption()
 			character := Character{
 				Name: addNewForm.GetFormItemByLabel("Name").(*tview.InputField).GetText(),
-				Init: addNewForm.GetFormItemByLabel("Init").(*tview.InputField).GetText(),
-				HP:   addNewForm.GetFormItemByLabel("HP").(*tview.InputField).GetText(),
+				Init: func() int {
+					i, _ := strconv.Atoi(addNewForm.GetFormItemByLabel("Init").(*tview.InputField).GetText())
+					return i
+				}(),
+				HP: func() int {
+					i, _ := strconv.Atoi(addNewForm.GetFormItemByLabel("HP").(*tview.InputField).GetText())
+					return i
+				}(),
 				Team: Team{
 					Id:   teamId,
 					Text: teamText,
 				},
 			}
 			AddNewCharacter(character)
-			// displayList.SetText("Current: " + character.Name + " (" + character.Init + ")")
-			displayList.AddItem(character.Name+" ("+character.Init+")", character.HP, 0, nil)
-			app.SetFocus(list)
+			app.SetFocus(commandList)
+			displayList.Clear()
+			for _, character := range GetCharactersSorted() {
+				lineItem := strconv.Itoa(character.Init) + ": " + character.Name + " (" + strconv.Itoa(character.HP) + ")"
+				displayList.AddItem(lineItem, "", 0, nil)
+			}
 		}).
 		AddButton("Clear", func() {
 			addNewForm.Clear(false)
 		})
 
-	list.AddItem("Add New", "", 'n', func() {
+	// List of options
+	commandList.AddItem("Add New", "", 'n', func() {
 		app.SetFocus(addNewForm)
 	})
-
-	addNewForm.SetHorizontal(true).SetBorder(true).SetTitle(" Add New ")
-	// .SetTitleAlign(tview.AlignLeft) // I like it but come back to this later
-	list.SetBorder(true).SetTitle(" List ")
 
 	// Set the input field to accept only numbers
 	addNewForm.GetFormItemByLabel("Init").(*tview.InputField).SetAcceptanceFunc(tview.InputFieldInteger)
 
-	// The layout
+	// Set the layout of components
 	flex := tview.NewFlex().SetDirection(tview.FlexRow).
 		AddItem(headerBox, 2, 1, false).
 		AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
-			AddItem(list, 0, 1, false).
+			AddItem(commandList, 0, 1, false).
 			AddItem(displayList, 0, 2, false), 0, 2, false).
 		AddItem(addNewForm, 5, 1, false)
 
 	// Run the application
-	if err := app.SetRoot(flex, true).SetFocus(list).Run(); err != nil {
+	if err := app.SetRoot(flex, true).SetFocus(commandList).Run(); err != nil {
 		panic(err)
 	}
 }
