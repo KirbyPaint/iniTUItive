054d18b884847c7a8d03287feb8d32c0b25d913d
Ash Porter <porter.ashley13@gmail.com>
2024-07-24 11:23:36 -0700

    add list functionality

--- main.go
+++ main.go
@@ -4,50 +4,66 @@ import (
 	"github.com/rivo/tview"
 )
 
-func AddNewCharacter(name, init, hp, team string) {
+// Team struct
+type Team struct {
+	Id   int
+	Text string
+}
+
+// Character struct
+type Character struct {
+	Name string
+	Init string
+	HP   string
+	Team Team
+}
+
+var characters []Character
+
+func AddNewCharacter(class Character) {
 	// Add a new character to the list
+	characters = append(characters, class)
 }
 
 func main() {
 	// The base application
 	app := tview.NewApplication()
 
-	headerBox := tview.NewBox().SetBorder(true).SetTitle(" Initiative Tracker ")
-	// listBox := tview.NewBox().SetBorder(true).SetTitle(" List ")
-	currentBox := tview.NewBox().SetBorder(true).SetTitle(" Current ")
-
+	headerBox := tview.NewBox()
+	displayList := tview.NewList()
+	list := tview.NewList()
 	addNewForm := tview.NewForm()
+
+	headerBox.SetBorder(true).SetTitle(" Initiative Tracker ")
+	displayList.SetBorder(true).SetTitle(" Current ")
+
 	addNewForm.AddInputField("Name", "", 20, nil, nil).
 		AddInputField("Init", "", 3, nil, nil).
 		AddInputField("HP", "", 4, nil, nil).
 		AddDropDown("Team", []string{"Player", "Ally", "Enemy", "Unknown"}, 0, nil).
 		AddButton("Save", func() {
-			// AddNewCharacter(addNewForm.GetFormItemByLabel("Name").(*tview.InputField).GetText(),
-			// 	addNewForm.GetFormItemByLabel("Init").(*tview.InputField).GetText(),
-			// 	addNewForm.GetFormItemByLabel("HP").(*tview.InputField).GetText(),
-			// 	addNewForm.GetFormItemByLabel("Team").(*tview.DropDown).GetCurrentOption())
-			app.SetFocus(currentBox)
+			teamId, teamText := addNewForm.GetFormItemByLabel("Team").(*tview.DropDown).GetCurrentOption()
+			character := Character{
+				Name: addNewForm.GetFormItemByLabel("Name").(*tview.InputField).GetText(),
+				Init: addNewForm.GetFormItemByLabel("Init").(*tview.InputField).GetText(),
+				HP:   addNewForm.GetFormItemByLabel("HP").(*tview.InputField).GetText(),
+				Team: Team{
+					Id:   teamId,
+					Text: teamText,
+				},
+			}
+			AddNewCharacter(character)
+			// displayList.SetText("Current: " + character.Name + " (" + character.Init + ")")
+			displayList.AddItem(character.Name+" ("+character.Init+")", character.HP, 0, nil)
+			app.SetFocus(list)
 		}).
 		AddButton("Clear", func() {
 			addNewForm.Clear(false)
 		})
 
-	// addNewForm := tview.NewForm().
-	// 	AddInputField("Name", "", 20, nil, nil).
-	// 	AddInputField("Init", "", 3, nil, nil).
-	// 	AddInputField("HP", "", 4, nil, nil).
-	// 	AddDropDown("Team", []string{"Player", "Ally", "Enemy", "Unknown"}, 0, nil).
-	// 	AddButton("Save", func() {
-	// 		app.SetFocus(currentBox)
-	// 	}).
-	// 	AddButton("Clear", func() {
-	// 		tview.NewForm().Clear(false)
-	// 	})
-
-	list := tview.NewList().
-		AddItem("Add New", "", 'n', func() {
-			app.SetFocus(addNewForm)
-		})
+	list.AddItem("Add New", "", 'n', func() {
+		app.SetFocus(addNewForm)
+	})
 
 	addNewForm.SetHorizontal(true).SetBorder(true).SetTitle(" Add New ")
 	// .SetTitleAlign(tview.AlignLeft) // I like it but come back to this later
@@ -61,7 +77,7 @@ func main() {
 		AddItem(headerBox, 2, 1, false).
 		AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
 			AddItem(list, 0, 1, false).
-			AddItem(currentBox, 15, 1, false), 0, 2, false).
+			AddItem(displayList, 0, 2, false), 0, 2, false).
 		AddItem(addNewForm, 5, 1, false)
 
 	// Run the application
