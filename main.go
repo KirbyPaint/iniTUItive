10498cefd206938e0676e7208e1e5aa2bc7e3c85
Ash Porter <porter.ashley13@gmail.com>
2024-07-24 10:44:59 -0700

    add more styling, gentle functionality

--- main.go
+++ main.go
@@ -4,26 +4,37 @@ import (
 	"github.com/rivo/tview"
 )
 
+func AddNewCharacter(name, init, hp, team string) {
+	// Add a new character to the list
+}
+
 func main() {
 	// The base application
 	app := tview.NewApplication()
 
 	headerBox := tview.NewBox().SetBorder(true).SetTitle(" Initiative Tracker ")
-	listBox := tview.NewBox().SetBorder(true).SetTitle(" List ")
+	// listBox := tview.NewBox().SetBorder(true).SetTitle(" List ")
 	currentBox := tview.NewBox().SetBorder(true).SetTitle(" Current ")
 
 	addNewForm := tview.NewForm().
-		AddInputField("Name", "", 0, nil, nil).
-		AddInputField("Init", "", 0, nil, nil).
-		AddInputField("HP", "", 0, nil, nil).
-		AddDropDown("Title", []string{"Player", "Ally", "Enemy", "Unknown"}, 0, nil).
+		AddInputField("Name", "", 20, nil, nil).
+		AddInputField("Init", "", 3, nil, nil).
+		AddInputField("HP", "", 4, nil, nil).
+		AddDropDown("Team", []string{"Player", "Ally", "Enemy", "Unknown"}, 0, nil).
 		AddButton("Save", func() {
+			app.SetFocus(currentBox)
 		}).
 		AddButton("Clear", func() {
 		})
 
+	list := tview.NewList().
+		AddItem("Add New", "", 'n', func() {
+			app.SetFocus(addNewForm)
+		})
+
 	addNewForm.SetHorizontal(true).SetBorder(true).SetTitle(" Add New ")
 	// .SetTitleAlign(tview.AlignLeft) // I like it but come back to this later
+	list.SetBorder(true).SetTitle(" List ")
 
 	// Set the input field to accept only numbers
 	addNewForm.GetFormItemByLabel("Init").(*tview.InputField).SetAcceptanceFunc(tview.InputFieldInteger)
@@ -32,12 +43,12 @@ func main() {
 	flex := tview.NewFlex().SetDirection(tview.FlexRow).
 		AddItem(headerBox, 2, 1, false).
 		AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
-			AddItem(listBox, 0, 1, false).
+			AddItem(list, 0, 1, false).
 			AddItem(currentBox, 15, 1, false), 0, 2, false).
-		AddItem(addNewForm, 8, 1, false)
+		AddItem(addNewForm, 5, 1, false)
 
 	// Run the application
-	if err := app.SetRoot(flex, true).SetFocus(addNewForm).Run(); err != nil {
+	if err := app.SetRoot(flex, true).SetFocus(list).Run(); err != nil {
 		panic(err)
 	}
 }
