c4bdd9c59b22d0a35339c5e08c3a89ec77be2969
KirbyPaint <porter.ashley13@gmail.com>
2024-08-10 16:29:50 -0700

    updated run script

--- run.sh
+++ run.sh
@@ -1,3 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-go run main.go
+# Note: be sure to add `.exe/` to your `.gitignore` file, otherwise this script will not work properly.
+
+set -euo pipefail
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+input_sha=$(
+    (
+        cd "$script_dir"
+        git ls-tree -r HEAD -- .
+        # shellcheck disable=SC2016
+        git status --short -uall . | cut -c4- | xargs -n1 /bin/sh -c 'sha1sum "$0" 2>&1 || true'
+    ) | sha1sum | cut -d' ' -f1
+)
+
+exe="$script_dir/.exe/$input_sha"
+
+[[ -x $exe ]] || {
+    echo "Compiling..."
+    mkdir -p .exe
+    go build -o "$exe" .
+}
+
+exec "$exe" "$@"
